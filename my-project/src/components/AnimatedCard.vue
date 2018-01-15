e688af09b1b886aca11dd121f4ae61cc51554a69
martin-banks <anotherbanksy@gmail.com>
2018-01-15 15:12:29 +1100

    adds new component

---
+++ my-project/src/components/AnimatedCard.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="test-card">
+		<h3>{{ label }} - {{ content.title.text }}</h3>
+
+		<transition name="anim">
+			<p v-if="seen">{{ content.par.text }}</p>
+		</transition>
+
+		<button
+			v-on:click="toggle"
+		>Toggle words</button>
+	</div>
+</template>
+
+
+<script>
+	export default {
+		name: 'animated-card',
+		props: ['content', 'label'],
+		data() {
+			return {
+				seen: false
+			}
+		},
+		methods: {
+			toggle() {
+				this.seen = !this.seen
+			},
+		},
+	}
+
+
+
+</script>
+
+<style modules lang="sass" scoped>
+	@import '../styleguide/index.sass'
+
+
+</style>
