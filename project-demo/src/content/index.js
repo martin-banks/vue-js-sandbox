27ad11145c96c171f14215edd42ed3d7a5629ef8
martin-banks <anotherbanksy@gmail.com>
2018-01-22 18:09:13 +1100

    moves content to separate file

---
+++ project-demo/src/content/index.js
@@ -0,0 +1,26 @@
+export default {
+	msg: 'Hello world',
+	facts: [
+		'Another very interesting fact',
+		'This fact is a fact'
+	],
+	images: [
+		{
+			src: 'http://via.placeholder.com/350x350',
+			alt: '350x150 Image caption',
+		},
+		{
+			src: 'http://via.placeholder.com/350x350',
+			alt: '350x150 Image caption',
+		},
+		{
+			src: 'http://via.placeholder.com/350x350',
+			alt: '350x150 Image caption',
+		},
+		{
+			src: 'http://via.placeholder.com/350x350',
+			alt: '350x150 Image caption',
+		},
+	]
+
+}
