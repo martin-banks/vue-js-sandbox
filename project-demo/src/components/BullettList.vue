6819f9aa5b2470fbd4b5bc31560f56d219953ec4
martin-banks <anotherbanksy@gmail.com>
2018-01-22 18:08:38 +1100

    new component for bullet list

---
+++ project-demo/src/components/BullettList.vue
@@ -0,0 +1,30 @@
+<template>
+	<ul>
+		<li v-for="(item, i) in list" :key="i">{{ item }}</li>
+	</ul>
+</template>
+
+
+<style scoped lang="sass">
+@import ../styleguide/index.sass
+
+ul
+	margin: 40px
+	+robotoCond-reg
+
+
+</style>
+
+
+<script>
+export default {
+	name: 'BullettList',
+	props: [
+		'list'
+	],
+	data() {
+		return {}
+
+	}
+}
+</script>
