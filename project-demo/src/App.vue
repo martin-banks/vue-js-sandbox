92b00e9b177bee8efd127e8b0d68846719c6725a
martin-banks <anotherbanksy@gmail.com>
2018-01-23 14:44:32 +1100

    moves factcard to own layout - reinstates router for demos

--- project-demo/src/App.vue
+++ project-demo/src/App.vue
@@ -1,14 +1,7 @@
 
 <template>
-  <div>
-    <fact-card>
-      <span slot="kicker">Custom kicker</span>
-      <h1 slot="title">{{ content.msg }}</h1>
-      <div slot="body">
-        <bullett-list :list="content.facts" />
-        <image-grid :images="content.images" />
-      </div>
-    </fact-card>
+  <div id="app">
+    <router-view />
   </div>
 </template>
 
@@ -21,24 +14,24 @@
 
 
 <script>
-import content from '@/content/index'
+// import content from '@/content/index'
 
-import FactCard from '@/components/FactCard'
-import ImageGrid from '@/components/ImageGrid'
-import BullettList from '@/components/BullettList'
+// import FactCard from '@/components/FactCard'
+// import ImageGrid from '@/components/ImageGrid'
+// import BullettList from '@/components/BullettList'
 // const FactCard = require('@/components/FactCard.vue')
 
 export default {
   name: 'App',
-  components: {
-    'fact-card': FactCard,
-    'image-grid': ImageGrid,
-    'bullett-list': BullettList,
-  },
-  data() {
-    return {
-      content
-    }
-  }
+  // components: {
+  //   'fact-card': FactCard,
+  //   'image-grid': ImageGrid,
+  //   'bullett-list': BullettList,
+  // },
+  // data() {
+  //   return {
+  //     // content
+  //   }
+  // }
 }
 </script>
