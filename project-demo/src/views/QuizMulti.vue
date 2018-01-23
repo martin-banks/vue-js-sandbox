d2285264ee53085f8537616bf3c13ec45840c313
martin-banks <anotherbanksy@gmail.com>
2018-01-23 14:47:27 +1100

    adds quiz layout

---
+++ project-demo/src/views/QuizMulti.vue
@@ -0,0 +1,29 @@
+<template>
+	<div class="quiz">
+		<quiz-multi />
+	</div>	
+</template>
+
+
+<style lang="sass" scoped>
+@import '../styleguide/index.sass'
+
+</style>
+
+<script>
+import QuizMulti from '@/components/QuizMulti'
+
+
+export default {
+	name: 'QuizMulti',
+	components: {
+		'quiz-multi': QuizMulti,
+	},
+	data() {
+		return {}
+	},
+
+}
+
+</script>
+
