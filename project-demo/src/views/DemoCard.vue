e030c5d7c3f241b14340ef822d4b62244f7c8a8a
martin-banks <anotherbanksy@gmail.com>
2018-01-23 14:47:10 +1100

    moves fact card to own loayout file

---
+++ project-demo/src/views/DemoCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <fact-card>
+      <span slot="kicker">Custom kicker</span>
+      <h1 slot="title">{{ content.msg }}</h1>
+      <div slot="body">
+        <bullett-list :list="content.facts" />
+        <image-grid :images="content.images" />
+      </div>
+    </fact-card>
+  </div>
+</template>
+
+
+<style scoped lang="sass">
+@import ../styleguide/index.sass
+
+
+</style>
+
+
+<script>
+import content from '@/content/index'
+
+import FactCard from '@/components/FactCard'
+import ImageGrid from '@/components/ImageGrid'
+import BullettList from '@/components/BullettList'
+
+export default {
+  name: 'DemoCard',
+  components: {
+    'fact-card': FactCard,
+    'image-grid': ImageGrid,
+    'bullett-list': BullettList,
+  },
+  data() {
+    return {
+      content
+    }
+  }
+}
+</script>
