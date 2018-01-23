33cd665c635158dae292e270a543248c4be44e0d
martin-banks <anotherbanksy@gmail.com>
2018-01-23 11:17:47 +1100

    updates styles

--- project-demo/src/components/FactCard.vue
+++ project-demo/src/components/FactCard.vue
@@ -1,9 +1,11 @@
 <template>
 	<div class="factCard">
-		<h6>
-			<slot name="kicker">Fact card</slot>
-		</h6>
-		<slot name="title" />
+		<div class="header">
+			<h6 class="kicker">
+				<slot name="kicker">Fact card</slot>
+			</h6>
+			<slot name="title" />
+		</div>
 		<div class="body" :style="bodyHeight" :data-status="bodyStatus">
 			<div class="wrapper">
 				<slot name="body" />
@@ -21,11 +23,20 @@
 	padding: 24px
 	+corner-round-1
 
+.header
+	margin-bottom: 24px
+
+.kicker
+	+robotoCond-bold
+	font-size: 14px
+	text-transform: uppercase
+	color: $color-blue-reg
+
 .body
 	transition: max-height 300ms
 	height: auto
 	overflow: hidden
-
+	margin-bottom: 24px
 
 button
 	padding: $space-1 $space-4
@@ -33,10 +44,8 @@ button
 	border: none
 	+corner-round-3
 	+robotoCond-reg
-	+fluidtype-1
+	font-size: 14px
 	color: $color-white
-
-
 
 
 
@@ -61,14 +70,11 @@ export default {
 				.getBoundingClientRect().height * 1.2
 			this.bodyStatus = this.bodyStatus === 'open' ? 'closed' : 'open'
 			this.bodyHeight = `max-height: ${this.bodyStatus === 'open' ? `${this.openHeight}px` : '0'}`
-			// console.log(this.openHeight)
 		}
 	},
 	created() {
-		console.log('created', this.$el)
 	},
 	mounted() {
-		console.log('el:', this.$el)
 		this.bodyStatus = 'closed'
 	}
 }
